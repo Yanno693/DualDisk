fbf7651cf6e13aa595e6f66fcc0b44a2e832d8f0
Yanno693 <ututa@hotmail.fr>
2021-02-02 16:38:37 +0100

    Ajout du saut

--- Assets/Dual Disk/Scripts/PlayerController.cs
+++ Assets/Dual Disk/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Camera currentCamera;
     public float movementSpeed;
     public float rotationSpeed;
+    public float mouvementY;
 
     private void FightingMovement()
     {
@@ -25,13 +26,22 @@ public class PlayerController : MonoBehaviour
         float angleBetween = Vector2.SignedAngle(Vector2.up, directionForward) * Mathf.Deg2Rad;
         Vector2 directionRotation = Vec2Rotate(directionInput, angleBetween);
 
-        characterController.Move(new Vector3(directionRotation.x, 0, directionRotation.y) * finalSpeed * Time.deltaTime);
+        mouvementY -= 6.0f * Time.deltaTime;
+
+        if(Input.GetButton("Jump") && characterController.isGrounded) {
+            Debug.Log("Jump !");
+            mouvementY = 2.5f;
+        }
+
+        //characterController.SimpleMove(new Vector3(directionRotation.x, jump, directionRotation.y) * finalSpeed * Time.deltaTime * 500.0f);
+        characterController.Move(new Vector3(directionRotation.x, mouvementY, directionRotation.y) * finalSpeed * Time.deltaTime);
+        //characterController.Sim
 
         transform.rotation = Quaternion.Lerp(
             transform.rotation,
             Quaternion.LookRotation(new Vector3(directionForward.x, 0, directionForward.y)),
             Time.deltaTime * rotationSpeed
-        );
+        );            
     }
 
     public static Vector2 Vec2Rotate(Vector2 v, float rad)
@@ -47,13 +57,18 @@ public class PlayerController : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
 
-        movementSpeed = 5.0f;
+        movementSpeed = 25.0f;
         rotationSpeed = 5.0f;
+
+        mouvementY = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         FightingMovement();
+
+        //if(Input.GetButton("Jump"))
+            //Debug.Log("Jump !");
     }
 }
