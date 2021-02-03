ff1c7b318b32bd342930af9ef9b09583220c2dbd
Bubbsy <virgile.aragon@gmail.com>
2021-02-03 10:12:23 +0100

    Spawn disk from player

---
+++ Assets/Scripts/PlayerThrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrow : MonoBehaviour
+{
+    public Transform spawn;
+    public GameObject prefabBall;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, speed, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -speed, 0);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(prefabBall, spawn.position, transform.rotation);
+        }
+    }
+}
