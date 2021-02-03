6463ce3644de8b5e6d4933a6b4b59ae492aee84c
Yanno693 <38687044+Yanno693@users.noreply.github.com>
2021-02-03 19:49:36 +0100

    Integration disk player (#5)
    * Debug integration disk - player - multi
    
    * Integration Joueur-disque-serveur pseudo finie
    
    * Maj NetworkManager

---
+++ Assets/Scripts/PlayerThrowIntegration.cs
@@ -0,0 +1,31 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrowIntegration : NetworkBehaviour
+{
+    //public Transform spawn;
+    public GameObject prefabBall;
+    public float speed;
+
+    [Command]
+    public void CmdDirac(Vector3 pos, Quaternion rot, Vector3 dir) {
+        Debug.Log("Dirac");
+
+        FindObjectOfType<NetworkManagerCustomIntegration>().SpawnProj(pos, rot, dir);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLocalPlayer) { 
+            if(Input.GetButtonDown("Fire1")) {
+                Transform camera_target = transform.GetChild(0);
+                Vector3 direction = (camera_target.position - GetComponent<NetworkPlayerController>().currentCamera.transform.position).normalized;
+
+                CmdDirac(transform.position + direction , transform.rotation, direction);
+            }
+        }
+    }
+}
