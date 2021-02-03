eba19b36ef8cf01d82edc5431b69d6009a20f5b7
Yanno693 <ututa@hotmail.fr>
2021-02-03 14:55:12 +0100

    Debug integration disk - player - multi

---
+++ Assets/Scripts/BounceIntegration.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceIntegration : MonoBehaviour
+{
+    public float speed;
+    public int max_rebond;
+
+    private Rigidbody rb;
+    private Vector3 target;
+    private int nb_rebond;  
+
+    // Start is called before the first frame update
+    public void setTarget(in Vector3 _target) {
+        this.target = _target;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //target = transform.TransformDirection(Vector3.forward);
+        //target = transform.forward;
+        nb_rebond = 0;
+
+        Debug.Log("Start ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = target * speed;
+        transform.LookAt(target, Vector3.up);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Touché");
+            Destroy(gameObject);
+        }
+
+        if(nb_rebond <= max_rebond)
+        {
+            var direction = Vector3.Reflect(target.normalized, collision.contacts[0].normal);
+
+            target = direction;
+            rb.velocity = target;
+            nb_rebond++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerThrowIntegration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrowIntegration : MonoBehaviour
+{
+    //public Transform spawn;
+    public GameObject prefabBall;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Transform camera_target = transform.GetChild(0);
+            
+            Vector3 direction = (camera_target.position - GetComponent<NetworkPlayerController>().currentCamera.transform.position).normalized;
+            GameObject proj = Instantiate<GameObject>(prefabBall, transform.position + direction, transform.rotation);
+            proj.GetComponent<BounceIntegration>().setTarget(direction);
+
+            Debug.Log(proj);
+        }
+    }
+}
