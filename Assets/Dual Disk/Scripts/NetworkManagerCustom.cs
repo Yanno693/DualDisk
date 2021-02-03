9da14c68658b94acd7bcb4a74dc1fc58ef30eda4
Yanno693 <38687044+Yanno693@users.noreply.github.com>
2021-02-03 10:52:55 +0100

    Network connection (#3)
    * Ajout d'une debut de serveur personallisé + connextion deux joueur avec camera perso
    
    * Ajout connection reseau (testé en local pour l'instant)

---
+++ Assets/Dual Disk/Scripts/NetworkManagerCustom.cs
@@ -0,0 +1,47 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManagerCustom : NetworkManager
+{
+    public struct PlayerConnectMessage : NetworkMessage {
+        public string data;
+    }
+
+    public override void OnStartServer() {
+        base.OnStartServer();
+
+        NetworkServer.RegisterHandler<PlayerConnectMessage>(OnCreateCharacter);
+    }
+    
+    // Start is called before the first frame update
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        // call this to use this gameobject as the primary controller
+        //NetworkServer.AddPlayerForConnection(conn, gameobject);
+
+        PlayerConnectMessage msg = new PlayerConnectMessage {
+            data = "Ok"
+        };
+        conn.Send(msg);
+    }
+
+    void OnCreateCharacter(NetworkConnection conn, PlayerConnectMessage message)
+    {
+        Debug.Log("Jello ?");
+        // playerPrefab is the one assigned in the inspector in Network
+        // Manager but you can use different prefabs per race for example
+        Camera c = FindObjectOfType<Camera>();
+        Debug.Log(c);
+        GameObject g = Instantiate<GameObject>(playerPrefab);
+
+        g.GetComponent<NetworkPlayerController>().currentCamera = c;
+        //Debug.Log(gameObject.GetComponent<NetworkPlayerController>().currentCamera);
+
+        // call this to use this gameobject as the primary controller
+        NetworkServer.AddPlayerForConnection(conn, g);
+    }
+}
