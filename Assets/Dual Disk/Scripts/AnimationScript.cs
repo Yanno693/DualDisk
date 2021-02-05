a876342dcbc874d4a975b8f5fb61393859d86d8f
Yanno693 <ututa@hotmail.fr>
2021-02-05 01:07:02 +0100

    Correction animation en multijoueur

--- Assets/Dual Disk/Scripts/AnimationScript.cs
+++ Assets/Dual Disk/Scripts/AnimationScript.cs
@@ -1,21 +1,33 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationScript : MonoBehaviour
+public class AnimationScript : NetworkBehaviour
 {
     private Animator animator;
+    private float throwWeight;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        throwWeight = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("X", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Y", Input.GetAxis("Vertical"));
+        if(isLocalPlayer) {
+            throwWeight -= Time.deltaTime;
+            animator.SetFloat("X", Input.GetAxis("Horizontal"));
+            animator.SetFloat("Y", Input.GetAxis("Vertical"));
+            animator.SetLayerWeight(animator.GetLayerIndex("Throw Layer"), throwWeight);
+
+            if(Input.GetButtonDown("Fire1")) {
+                throwWeight = 1.0f;
+                animator.Play("Throw_Revert", animator.GetLayerIndex("Throw Layer"), 0.0f);
+            }
+        }
     }
 }
