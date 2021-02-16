124f8fc91c579e107d3b327757466fa5ce916eae
Bubbsy <virgile.aragon@gmail.com>
2021-02-16 15:12:28 +0100

    resolve conflits

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cam.GetComponent<Cinemachine.CinemachineFreeLook>().enabled = false;
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+}
